f1d61f2a71d0c2e17168501cfac6995729ebcd01
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2022-11-16 03:51:23 +0900

    サンプルレシピデータを返すAPIを作成

--- api/controller/recipe_controller.go
+++ api/controller/recipe_controller.go
@@ -6,8 +6,35 @@ import (
   "github.com/gin-gonic/gin"
 )
 
+// レシピデータの構造体
+type recipeData struct {
+  Name string `json:name`
+  Img string `json:img`
+}
+
+// MYレシピデータの構造体
+type myRecipeData struct {
+	Monday []recipeData
+	Tuesday []recipeData
+	Wednesday []recipeData
+	Thursday []recipeData
+	Friday []recipeData
+	Saturday []recipeData
+	Sunday []recipeData
+}
+
 func RecipeDataGet(c *gin.Context) {
-  c.String(http.StatusOK, "献立一覧取得API")
+  // レシピデータ
+  recipeData := []recipeData{
+    {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+    {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+    {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+    {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+    {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+    {Name: "卵かけご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+    {Name: "美味しい煮物", Img: "../../assets/images/nimono.jpg"},
+	}
+  c.JSON(http.StatusOK, recipeData)
 }
 
 func MyRecipeDataSave(c *gin.Context) {
@@ -15,7 +42,58 @@ func MyRecipeDataSave(c *gin.Context) {
 }
 
 func MyRecipeDataGet(c *gin.Context) {
-  c.String(http.StatusOK, "my献立取得API")
+	myRecipeData := myRecipeData{
+    Monday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Tuesday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵かけご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Wednesday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Thursday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Friday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Saturday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+    Sunday: []recipeData{
+      {Name: "唐揚げ", Img: "../../assets/images/karaage.jpg"},
+      {Name: "サラダ", Img: "../../assets/images/sarada.jpg"},
+      {Name: "煮物", Img: "../../assets/images/nimono.jpg"},
+      {Name: "卵をかけたご飯", Img: "../../assets/images/tamagokakegohan.jpg"},
+      {Name: "味噌汁", Img: "../../assets/images/misoshiru.jpg"},
+		},
+  }
+  c.JSON(http.StatusOK, myRecipeData)
 }
 
 func RecipeCreateSettingGet(c *gin.Context) {
