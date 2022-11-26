7a2c80a6deb6ba54c790c6bd51ebcd0e3e96ffbd
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2022-11-26 19:52:10 +0900

    マイグレーション修正

--- api/model/food.go
+++ api/model/food.go
@@ -9,7 +9,7 @@ type Food struct {
 	gorm.Model
 	Name string `gorm:"type:varchar(100);not null;comment:'食材名';"`
 	Amazon_id string `gorm:"type:varchar(100);not null;comment:'amazonの商品ID';"`
-	gram uint `gorm:"type:int(11);comment:'食材の量（グラム）';"`
-	quantity uint `gorm:"type:int(11);comment:'食材の量（数）';"`
-	price uint `gorm:"type:int(11);not null;comment:'食材の値段';"`
+	Gram uint `gorm:"type:int(11);comment:'食材の量（グラム）';"`
+	Quantity uint `gorm:"type:int(11);comment:'食材の量（数）';"`
+	Price uint `gorm:"type:int(11);not null;comment:'食材の値段';"`
 }
--- api/model/recipe_material.go
+++ api/model/recipe_material.go
@@ -9,8 +9,8 @@ type Recipe_material struct {
 	gorm.Model
 	RecipeID uint `gorm:"type:int(11);not null;comment:'レシピID';"`
 	FoodID uint `gorm:"type:int(11);not null;comment:'食材ID';"`
-	grams uint `gorm:"type:int(11);comment:'食材の量（グラム）';"`
-	quantity uint `gorm:"type:int(11);comment:'食材の量（数）';"`
-	quantity_label string `gorm:"type:varchar(20);comment:'食材の量（ラベル）';"`
+	Gram uint `gorm:"type:int(11);comment:'食材の量（グラム）';"`
+	Quantity uint `gorm:"type:int(11);comment:'食材の量（数）';"`
+	Quantity_label string `gorm:"type:varchar(20);comment:'食材の量（ラベル）';"`
 	Food Food
 }
