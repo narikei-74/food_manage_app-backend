78ab553c69bfbef9df9c623fdb38e3bf7bac0fd7
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2022-11-26 20:47:57 +0900

    マイグレーション修正

--- api/model/recipe.go
+++ api/model/recipe.go
@@ -12,6 +12,7 @@ type Recipe struct {
 	Image_key string `gorm:"type:varchar(100);not null;comment:'レシピ画像のキー';"`
 	Dish_category uint `gorm:"type:tinyint(1);not null;comment:'栄養バランスカテゴリ。1が「主食」、2が「主菜」、3が「副菜」、4が「汁物」。';"`
 	UserID uint `gorm:"type:int(11);comment:'レシピ製作者。nullの場合はオリジナルレシピ。';"`
+	How_to_cook string `gorm:"type:text;comment:'作り方';"`
 	Recipe_materials []Recipe_material
 	Recipe_categories []Recipe_category
 }
