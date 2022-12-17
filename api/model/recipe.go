2c44bb6128a8f3a434351fe8c563a9498d8fe25e
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2022-12-17 22:04:49 +0900

    recipeテーブル 公開に関するカラム追加

--- api/model/recipe.go
+++ api/model/recipe.go
@@ -13,6 +13,8 @@ type Recipe struct {
 	Dish_category uint `gorm:"type:tinyint(1);not null;comment:'栄養バランスカテゴリ。1が「主食」、2が「主菜」、3が「副菜」、4が「汁物」。';"`
 	UserID uint `gorm:"type:int(11);comment:'レシピ製作者。nullの場合はオリジナルレシピ。';"`
 	How_to_cook string `gorm:"type:text;comment:'作り方';"`
+	Private_flag uint `gorm:"type:tinyint(1);not null;comment:'プライベートフラグ。0が公開、1が非公開。'";`
+	Is_ok_public uint `gorm:"type:tinyint(1);not null;comment:'公開してもしてもいいか。0が公開不可、1が公開可。'";`
 	Recipe_materials []Recipe_material
 	Recipe_categories []Recipe_category
 }
