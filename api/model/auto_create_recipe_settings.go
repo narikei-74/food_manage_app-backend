9764a311b41cec82aba222fc7fba59c22759ec1b
narikei_74 <uc9pr8cztud79bqm8g2v@docomo.ne.jp>
2023-01-09 18:35:51 +0900

    モデル修正

--- api/model/auto_create_recipe_settings.go
+++ api/model/auto_create_recipe_settings.go
@@ -9,7 +9,7 @@ type Auto_create_recipe_settings struct {
 	gorm.Model
 	UserID uint `gorm:"type:int(11);not null;comment:'ユーザーID';"`
 	Hate_foods string `gorm:"type:text;comment:'アレルギー・嫌いな食材。jsonでfoodIDを持つ';"`
-	is_only_rice uint `gorm:"type:tinyint(1);not null;default:0;comment:'主食を白米のみにするかどうか';"`
-	is_only_meat uint `gorm:"type:tinyint(1);not null;default:0;comment:'主菜を肉料理のみにするかどうか';"`
-	is_only_fish uint `gorm:"type:tinyint(1);not null;default:0;comment:'主菜を魚料理のみにするかどうか';"`
+	Is_only_rice uint `gorm:"type:tinyint(1);not null;default:0;comment:'主食を白米のみにするかどうか';"`
+	Is_only_meat uint `gorm:"type:tinyint(1);not null;default:0;comment:'主菜を肉料理のみにするかどうか';"`
+	Is_only_fish uint `gorm:"type:tinyint(1);not null;default:0;comment:'主菜を魚料理のみにするかどうか';"`
 }
